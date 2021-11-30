e0c2f4f87a1a9e582dd5af0b0612d37e03101719
Njuguna Mwaura <coder866@gmail.com>
2021-11-30 18:28:47 +0300

    Minor adjustments

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,15 +5,43 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function register(Request $request)
     {
-        # code...
+        $this->validate($request,[
+        "name"=>"required|min:3",
+        "email"=>"required|email|unique:users",
+        "password"=>"required|confirmed",
+        "password_confirmation"=>"required"
+        ]);
+
+        $user=User::create([
+        'name'=>$request->name,
+        'email'=>$request->email,
+        'password'=>Hash::make($request->password)
+        ]);
+        $resp=['User Created Successfully',$user];
+
+        return $resp;
     }
     public function login(Request $request)
     {
-        # code...
+        $credentials = $request->validate([
+        'email' => ['required', 'email'],
+        'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+        $token=$request->session()->regenerateToken();
+        return ['LoggedIn Successfully',$token];
+        }
+
+        return back()->withErrors([
+        'email' => 'The provided credentials do not match our records.',
+        ]);
     }
 }
